cc84f9794e5ea82622669ade3c07ace0cf3ed3cb
widedfajraoui <wided.fajraoui74@gmail.com>
2023-06-07 17:13:52 +0100

    first commit

---
+++ src/components/UserList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <h1>User List</h1>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.firstName }} {{ user.lastName }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'; // Import Axios library
+  
+  export default {
+    data() {
+      return {
+        users: [],
+      };
+    },
+    mounted() {
+      // Fetch users from the API when the component is mounted
+      this.fetchUsers();
+    },
+    methods: {
+      fetchUsers() {
+        // Make an API call to retrieve the users
+        axios
+          .get('http://localhost:4000/user/All')
+          .then(response => {
+            this.users = response.data;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add your custom styles here */
+  </style>
+  
